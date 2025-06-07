160a63bd842bbaa072ad47778c5d3f144ca97221
Muhammad Bilal Hussain <bilalhussain42201@gmail.com>
2025-06-07 12:36:37 -0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from ch-03-fast-api!")
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/username/{username}/{user_id}")
+def read_user(
+    username: str,
+    user_id: int = Path(..., gt=1, lt=100)
+):
+    return {"username": username, "user_id": user_id}
+
+@app.get("/username/junaid")
+def read_username_junaid():
+    return {"username": "Junaid Hussain"}
+
+@app.get("/username/bilal?token=1234&")
+def read_username():
+    return {"username": "Bilal"}
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/username/all")
+def read_all_usernames(limit:int | None = None):
+    print(f"Limit: {limit}")
+    if limit is not None:
+        return {"usernames": ["Junaid Hussain"]}
+    else:
+        return {"usernames": ["Junaid Hussain", "Bilal"]}
